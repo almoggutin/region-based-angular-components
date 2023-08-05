1d78c5084e3ae9ad8e95216e42fcbaf35facef9e
Almog Gutin <almogm.gutin@gmail.com>
2023-08-05 18:05:50 +0300

    Added basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'region-based-components';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const APP_ROUTES: Routes = [];
+export const APP_ROUTES: Routes = [
+	{
+		path: '',
+		loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+	},
+	{
+		path: '**',
+		redirectTo: '',
+	},
+];
