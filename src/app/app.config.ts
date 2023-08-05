a9177b9ed4040ca867887f4ea0670694032d12da
Almog Gutin <almogm.gutin@gmail.com>
2023-08-05 17:37:59 +0300

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+	providers: [provideRouter(APP_ROUTES, withComponentInputBinding(), withHashLocation()), provideHttpClient()],
 };
