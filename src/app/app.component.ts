228ec835118315547c8ea08eda89c7f3f3dfe946
Almog Gutin <almogm.gutin@gmail.com>
2023-08-05 19:58:00 +0300

    Added buttons to change the region

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf, UpperCasePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { tap, Observable } from 'rxjs';
+
+import { LocalizationService } from './services';
 
 import { HeaderComponent } from './components/header/header.component';
+
+import { Region } from './models';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, HeaderComponent],
+	imports: [NgIf, AsyncPipe, UpperCasePipe, RouterOutlet, HeaderComponent],
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+	private readonly localizationService = inject(LocalizationService);
+
+	region$: Observable<Region> = this.localizationService.region$;
+
+	Region = Region;
+
+	handleRegionChange(region: Region): void {
+		this.localizationService.setLocale({ region });
+	}
+}
